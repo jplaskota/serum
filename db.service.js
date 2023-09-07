86901a74a6ee9773d439aaa265b6262df79b466c
jurandplaskota <jurand.plaskota@microsoft.wsei.edu.pl>
2023-09-07 22:21:59 +0200

    crud updated

--- db.service.js
+++ db.service.js
@@ -3,7 +3,7 @@ import { MongoClient, ObjectId } from "mongodb";
 
 dotenv.config();
 
-const collections = {};
+export const collections = {};
 
 export async function connectToDatabase() {
   const client = new MongoClient(process.env.DB_CONN_STRING);
@@ -14,8 +14,10 @@ export async function connectToDatabase() {
     const db = client.db("SerumProject");
 
     collections.notes = db.collection("notes");
-  } catch (e) {
-    console.error("DB connection status: failed: ", e);
+
+    // deleteNote("64f90d76ed5732eb362adaa8");
+  } catch (err) {
+    console.error("DB connection status: failed: ", err);
   } finally {
     console.log("DB connection status: success");
   }
@@ -33,25 +35,36 @@ export async function addNote(newNote) {
 }
 
 export async function updateNote(id, newNote) {
-  await collections.notes.updateOne(
-    { _id: new ObjectId(id) },
-    { $set: newNote },
-    (err, result) => {
-      if (err) throw err;
-      console.log("Note edited: ", result);
+  try {
+    const result = await collections.notes.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: newNote }
+    );
+
+    if (result && result.updateCount) {
+      console.log("Note updated: ", result);
+    } else if (!result.updateCount) {
+      console.log("Can't find note with this id: ", result);
+    } else {
+      console.log("Note has not been updated", result);
     }
-  );
+  } catch (err) {
+    console.error("Problem with updating note: ", err);
+  }
 }
 
 export async function deleteNote(id) {
-  await collections.notes.deleteOne({ _id: ObjectId(id) }, (err, result) => {
-    if (err) throw err;
-    if (result && result.deletedCount) {
+  try {
+    const result = await collections.notes.deleteOne({ _id: new ObjectId(id) });
+
+    if (result && result.deleteCount) {
       console.log("Note deleted: ", result);
-    } else if (!result?.deletedCount) {
+    } else if (!result.deleteCount) {
       console.log("Can't find note with this id: ", result);
     } else {
       console.log("Note has not been removed: ", result);
     }
-  });
+  } catch (err) {
+    console.error("Problem with deleting note: ", err);
+  }
 }
